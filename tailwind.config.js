63aa4ea95fb48debea9104ea6232399020b3c764
Thales <thalesgoncalves3@gmail.com>
2024-03-28 14:49:36 -0300

    criando aplicação de notas com laravel e tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    colors: {
+      gray: colors.red,
+      blue: colors.red,
+      red: colors.red,
+      pink: colors.red,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ]
+}
+
