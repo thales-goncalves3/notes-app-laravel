0646e412bbcf92aee6cd8eddeba3dbe939f72d22
Thales <thalesgoncalves3@gmail.com>
2024-03-29 20:35:29 -0300

    adicionando novos estilos

--- resources/views/criar-tarefa.blade.php
+++ resources/views/criar-tarefa.blade.php
@@ -5,29 +5,28 @@
     @include('layouts.head')
 </head>
 
-<body>
+<body class="flex flex-col h-screen">
     @include('layouts.header')
 
+    <div class="flex flex-col items-center justify-center flex-1 bg-gray-200">
+        <form class=" w-full max-w-lg bg-white shadow-md rounded-lg overflow-hidden" method="POST" action="{{ route('dashboard') }}">
+            @csrf
 
-    <form class="max-w-sm mx-auto" method="POST" action="{{ route('dashboard') }}">
+            <div class="p-6">
+                <div class="mb-5">
+                    <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Título</label>
+                    <input name="title" type="text" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </div>
 
-        @csrf
+                <div class="mb-5">
+                    <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descrição</label>
+                    <input name="description" type="text" id="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </div>
 
-
-        <div class="mb-5">
-            <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Título</label>
-            <input name="title" type="text" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-        </div>
-        <div class="mb-5">
-            <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descrição</label>
-            <input name="description" type="text" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-        </div>
-
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Salvar</button>
-
-
-    </form>
-
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Salvar</button>
+            </div>
+        </form>
+    </div>
 
     @include('layouts.footer')
 </body>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,27 +1,15 @@
 
 
 @yield('navbar')
-<nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-    </a>
-    <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-      <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-        <li>
-          <a href="/" class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Tarefas criadas</a>
-        </li>
-        <li>
-          <a href="/nova-tarefa" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Criar Tarefa</a>
-        </li>
-      </ul>
+<nav class="bg-gray-800 p-4">
+    <div class="flex justify-between items-center">
+        <div>
+            <a href="/" class="text-white font-semibold text-lg">Notes App</a>
+        </div>
+        <div>
+            <a href="/" class="nav-link text-gray-300 hover:text-white mr-4 {{ request()->is('/') ? 'text-white border-b-2 border-white' : '' }}" >Tarefas Criadas</a>
+            <a href="/nova-tarefa" class="nav-link text-gray-300 hover:text-white {{ request()->is('nova-tarefa') ? 'text-white border-b-2 border-white ' : '' }}" >Criar Tarefa</a>
+        </div>
     </div>
-  </div>
 </nav>
+
